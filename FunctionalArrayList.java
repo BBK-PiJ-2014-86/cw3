277715abcfd3d6ff2f98ae5abf18a2febbece361
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-20 10:00:36 +0000

    initial version

---
+++ FunctionalArrayList.java
@@ -0,0 +1,18 @@
+package cw3;
+
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+
+	@Override
+	public ReturnObject head() {
+	
+		return null;
+	}
+
+	@Override
+	public FunctionalList rest() {
+
+		return null;
+	}
+
+	
+}
