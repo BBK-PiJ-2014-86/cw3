022c885637eddab5a82e133101d65420f0de463d
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-20 10:02:31 +0000

    initial version

---
+++ FunctionalLinkedList.java
@@ -0,0 +1,17 @@
+package cw3;
+
+public class FunctionalLinkedList extends LinkedList implements FunctionalList {
+
+	@Override
+	public ReturnObject head() {
+
+		return null;
+	}
+
+	@Override
+	public FunctionalList rest() {
+
+		return null;
+	}
+
+}
