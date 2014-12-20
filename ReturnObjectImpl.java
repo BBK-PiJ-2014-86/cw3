30727105d16c0a58c69c638f8ee403a3ee5c92b0
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-20 09:40:29 +0000

    Initial version

---
+++ ReturnObjectImpl.java
@@ -0,0 +1,25 @@
+package cw3;
+
+public class ReturnObjectImpl implements ReturnObject {
+
+	@Override
+	public boolean hasError() {
+		
+		return false;
+	}
+
+	@Override
+	public ErrorMessage getError() {
+		
+		return null;
+	}
+
+	@Override
+	public Object getReturnValue() {
+		
+		return null;
+	}
+	
+	
+
+}
