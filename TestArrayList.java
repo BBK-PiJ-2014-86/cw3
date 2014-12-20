8d87f387637402499e06b118f5eb5e7aba83bff0
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-20 17:58:15 +0000

    testing the ArrayList 1pt
    - add
    - size
    -isEmpty()

---
+++ TestArrayList.java
@@ -0,0 +1,23 @@
+package cw3;
+
+public class TestArrayList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ArrayList array = new ArrayList();
+		
+		array.add("Andrean");
+		array.add("Mike Sandison");
+		array.add("Bob Dylan");
+		
+		System.out.println(array.size());
+		System.out.println(array.isEmpty());
+		
+	}
+
+}
