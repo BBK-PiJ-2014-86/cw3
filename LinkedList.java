f86f040a0a385d271b89519a99767d783e4eb32a
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-20 09:57:46 +0000

    initial version

---
+++ LinkedList.java
@@ -0,0 +1,41 @@
+package cw3;
+
+public class LinkedList implements List {
+
+	@Override
+	public boolean isEmpty() {
+
+		return false;
+	}
+
+	@Override
+	public int size() {
+		
+		return 0;
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+
+		return null;
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+
+		return null;
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+
+		return null;
+	}
+
+}
