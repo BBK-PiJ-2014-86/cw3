3ed91c217c7b57df4c38a445b4354a3b98471d30
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-20 10:07:55 +0000

    initial version

---
+++ StackImpl.java
@@ -0,0 +1,41 @@
+package cw3;
+
+public class StackImpl extends AbstractStack{
+
+	public StackImpl(List list) {
+		super(list);
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+
+		return false;
+	}
+
+	@Override
+	public int size() {
+
+		return 0;
+	}
+
+	@Override
+	public void push(Object item) {
+
+		
+	}
+
+	@Override
+	public ReturnObject top() {
+		
+		return null;
+	}
+
+	@Override
+	public ReturnObject pop() {
+		
+		return null;
+	}
+
+	
+}
