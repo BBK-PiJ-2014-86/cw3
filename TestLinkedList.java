a9af0897f9ea833058d5580360c3635ce01ceb8e
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-27 15:20:32 +0000

    initial version
    testing linked list add and get methods

---
+++ TestLinkedList.java
@@ -0,0 +1,19 @@
+package cw3;
+
+public class TestLinkedList {
+
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		
+		list.add("Amber");
+		list.add("Geogaddi");
+		list.add("SAW 2");
+		list.add("Untrue");
+		
+		System.out.println(list.get(1).getReturnValue());
+
+	}
+
+}
