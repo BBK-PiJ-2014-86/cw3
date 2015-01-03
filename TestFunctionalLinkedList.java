ba90e262075f9fae3390c9e796537e8e8700b96d
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-01-03 18:42:40 +0000

    testing FunctionalLinkedList

---
+++ TestFunctionalLinkedList.java
@@ -0,0 +1,21 @@
+package cw3;
+
+public class TestFunctionalLinkedList {
+
+	public static void main(String[] args) {
+
+	FunctionalList testList = new FunctionalArrayList();
+		
+		testList.add("Ada Lovelace");
+		testList.add("Charles Babbage");
+		testList.add("Vannevar Bush");
+		testList.add("Mauchly");
+		testList.add("Licklider");
+		
+		System.out.println(testList.head().getReturnValue());
+		System.out.println(testList.rest().size());
+		System.out.println(testList.rest().get(3).getReturnValue());
+		
+	}
+
+}
