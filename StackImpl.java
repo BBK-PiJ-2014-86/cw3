759f2ee94562d230ef6490997f4be5750d7ae507
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-02-01 14:33:56 -0600

    StackImpl changes
    - implemented isEmpty() method;
    - implemented size();

--- StackImpl.java
+++ StackImpl.java
@@ -1,34 +1,45 @@
 package cw3;
 
 public class StackImpl extends AbstractStack{
+	
+	// do I create size param to track the size of the stack or not? 
+	// size of the list must be visible - getSize?
 
+	//what if the list is empty?
+	//there is a thing called internalList which is of type List;
+	
 	public StackImpl(List list) {
 		super(list);
+		
 
 	}
 
 	@Override
 	public boolean isEmpty() {
 
-		return false;
+		if (internalList.size()==0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public int size() {
 
-		return 0;
+		return internalList.size();
 	}
 
 	@Override
 	public void push(Object item) {
-
-		
+ 
+		  internalList.add(item);
 	}
 
 	@Override
 	public ReturnObject top() {
 		
-		return null;
+		return internalList.get(internalList.size()-1);
 	}
 
 	@Override
