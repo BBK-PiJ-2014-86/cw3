73cb18081d5647789b396546ffe9d902facb8762
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-02-01 15:50:25 -0600

    testing continued
    - fixed tests testPopEmpty() and testTopEmpty () to check against the
    appropriate error message object (EMPTY_STRUCTURE)
    -  testPushIntoEmpty - this would test if push() will add element to
    the empty List
    - testSize() checks if the size is correct
    - testPushExtreme tests if the Stack can deal with 1 million objects in
    the stack

--- TestStackImpl.java
+++ TestStackImpl.java
@@ -28,10 +28,14 @@ public class TestStackImpl {
 		
 	}
 	
+	/**
+	 * This test tests if popping empty stack would return ErrorMessage object of type EMPTY_STRUCTURE 
+	 */
+	
 	@Test
 	public void testPopEmpty() {
 	
-		assertTrue(myStack.pop().hasError());
+		assertEquals(myStack.pop().getError(), ErrorMessage.EMPTY_STRUCTURE);
 		
 	}
 	
@@ -47,13 +51,47 @@ public class TestStackImpl {
 		
 		assertEquals(myStack.top().getReturnValue(),"6");
 		
-		
 	}
 	
 	@Test
 	public void testTopEmpty() {
 		
-		assertTrue(myStack.pop().hasError());
+		assertEquals(myStack.top().getError(), ErrorMessage.EMPTY_STRUCTURE);
+		
+	}
+	
+	@Test
+	public void testPushIntoEmpty() {
+		
+		myStack.push("1");
+		
+		assertEquals(myStack.top().getReturnValue(), "1");
+		
+	}
+	
+	@Test 
+	public void testSize() {
+		
+		myStack.push("1");
+		myStack.push("2");
+		myStack.push("3");
+		myStack.push("4");
+		myStack.push("5");
+		myStack.push("6");
+		
+		assertEquals(myStack.size(),6);
+		
+	}
+	
+	@Test
+	public void testPushExtreme () {
+		
+		for (int i = 0; i<1000000; i++) {
+			String myString = ""+i;
+			myStack.push(myString);
+		}
+		
+		assertEquals(myStack.pop().getReturnValue(),"999999");
 		
 	}
 
