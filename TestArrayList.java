7eba31a4c1579d86a02f2901684e6f471fb361c8
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-02-02 17:19:32 -0600

    changed testing to Junit

--- TestArrayList.java
+++ TestArrayList.java
@@ -1,42 +1,86 @@
 package cw3;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestArrayList {
 
+	List myList;
+		
+	@Before
+	public void initialise () {
+		myList = new ArrayList ();
+	}
+
+	@Test
+	public void testaAddAndGetNormal() {
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
+		
+		assertEquals(myList.get(5).getReturnValue(),"6");
+		
+	}
+	
 	/**
-	 * @param args
+	 * This test tests if popping empty stack would return ErrorMessage object of type EMPTY_STRUCTURE 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	@Test
+	public void testGetEmpty() {
+	
+		assertEquals(myList.get(1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		
+	}
+	
+	@Test
+	public void testAddNormal () {
+	
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add(3,"6");
+		
+		assertEquals(myList.get(3).getReturnValue(),"6");
+		
+	}
 
 		
-		ArrayList array = new ArrayList();
-		
-		array.add("Andrean");
-		array.add("Mike Sandison");
-		array.add("Bob Dylan");
-		
-		System.out.println(array.size());
-		System.out.println(array.isEmpty());
 
+	@Test 
+	public void testSize() {
 		
-
-		System.out.println(array.get(1).getReturnValue().toString());
-		System.out.println(array.remove(0).getReturnValue().toString());
-		array.add(0, "Happy Cycling");
-		array.add(0, "Dayvan Cowboy");
-		array.add(0, "Avril 14");
-		array.add(0, "An Eagle in your mind");
-		array.add(0, "1969");
-		array.add(0, "Rhubarb");
-		array.add(0, "444");
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
 		
-		System.out.println(array.get(1).getReturnValue().toString());
-		System.out.println(array.get(6).getReturnValue().toString());
+		assertEquals(myList.size(),6);
 		
-		System.out.println(array.size());
+	}
+	
+	@Test
+	public void testAddExtreme () {
 		
+		for (int i = 0; i<1000000; i++) {
+			String myString = ""+i;
+			myList.add(myString);
+		}
+		
+		assertEquals(myList.get(999999).getReturnValue(),"999999");
+		
+	}
 
 		
 	}
 
-}
