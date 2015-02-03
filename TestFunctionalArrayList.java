186fdea98f1054ab4fd47f868a9376b8454c7619
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-02-02 18:18:17 -0600

    adding Junit Testing

--- TestFunctionalArrayList.java
+++ TestFunctionalArrayList.java
@@ -1,23 +1,142 @@
 package cw3;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestFunctionalArrayList {
 
-
-	public static void main(String[] args) {
+	List myList;
 		
-		FunctionalList testList = new FunctionalArrayList();
-		
-		testList.add("Ada Lovelace");
-		testList.add("Charles Babbage");
-		testList.add("Vannevar Bush");
-		testList.add("Mauchly");
-		testList.add("Licklider");
-		
-		System.out.println(testList.head().getReturnValue());
-		System.out.println(testList.rest().size());
-		System.out.println(testList.rest().get(1).getReturnValue());
-
-
+	@Before
+	public void initialise () {
+		myList = new FunctionalArrayList ();
 	}
 
-}
+	@Test
+	public void testaAddAndGetNormal() {
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
+		
+		assertEquals(myList.get(5).getReturnValue(),"6");
+		
+	}
+	
+	
+	@Test
+	public void testGetEmpty() {
+	
+		assertEquals(myList.get(1).getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		
+	}
+	
+	@Test
+	public void testIsEmptyAfterAddingAndRemoving() {
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
+		
+		myList.remove(0);
+		myList.remove(0);
+		myList.remove(0);
+		myList.remove(0);
+		myList.remove(0);
+		myList.remove(0);
+		
+	
+		assertEquals(0,myList.size());
+		
+	}
+	
+	
+	
+	@Test
+	public void testAddNormal () {
+	
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add(3,"6");
+		
+		assertEquals(myList.get(3).getReturnValue(),"6");
+		
+	}
+
+		
+
+	@Test 
+	public void testSize() {
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
+		
+		assertEquals(myList.size(),6);
+		
+	}
+	
+	@Test
+	public void testAddExtreme () {
+		
+		for (int i = 0; i<1000000; i++) {
+			String myString = ""+i;
+			myList.add(myString);
+		}
+		
+		assertEquals(myList.get(999999).getReturnValue(),"999999");
+		
+	}
+	
+	@Test
+	public void TestHead(){
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
+		
+		assertEquals("1",((FunctionalArrayList)myList).head().getReturnValue().toString());
+		
+	}
+	
+	@Test
+	public void TestRest(){
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.add("6");
+		
+		FunctionalList myList2 = new FunctionalArrayList ();
+		myList2.add("2");
+		myList2.add("3");
+		myList2.add("4");
+		myList2.add("5");
+		myList2.add("6");
+		
+		assertEquals(myList.get(1).getReturnValue(),myList2.get(0).getReturnValue());
+		
+	}
+
+		
+	}
+
